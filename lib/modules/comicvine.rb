892cbc618727ed696616396ec5f8aeda12678b24
karynntran <karynn.tran@gmail.com>
2014-12-04 12:13:25 -0500

    Update API routes to get all names that match a query

--- lib/modules/comicvine.rb
+++ lib/modules/comicvine.rb
@@ -1,7 +1,12 @@
 module ComicVine
 
   def self.get_characters
-
+    character =
+    query =
+    url = "http://comicvine.com/api/search/?api_key=#{api_key}&resources=character&query=#{query}&field_list=name,origin,publisher")
+    api_response = HTTParty.get(url)
+    results = api["response"]["results"]["character"]
+    all_characters = results.map { |result| result["name"] }
   end
 
   def self.character_stats
