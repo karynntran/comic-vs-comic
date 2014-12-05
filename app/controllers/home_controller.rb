bed2504e6e0ace8920a589563056e96881a15e75
karynntran <karynn.tran@gmail.com>
2014-12-04 20:47:05 -0500

    Update module methods in ComicVine

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,18 @@ class HomeController < ApplicationController
   end
 
   def search
+
     query = params['query']
     @characters = Character.all
     api_character = ComicVine.get_character(query)
+
+    binding.pry
+
+    hash = ComicVine.character_stats
       if @characters.include?(api_character)
         character = Character.find_by(name:query)
       elsif api_character
-        character = Character.create({name: api_character.name...etc})
+        character = Character.create(hash)
       else
         character = nil
       end
--- lib/modules/comic_vine.rb
+++ lib/modules/comic_vine.rb
@@ -4,10 +4,18 @@ module ComicVine
     all_characters_url = "http://comicvine.com/api/characters/?api_key=#{api_key}&field_list=name,id,api_detail_url&filter=name:#{query}"
     api_all_characters = HTTParty.get(all_characters_url)
     first_result_id = api_all_characters["response"]["results"]["character"][0]["id"]
-
-    #grab single character
     one_character_url = "http://comicvine.com/api/character/4005-#{first_result_id}?api_key=#{api_key}&field_list=name,powers,id,image,team_friends,team_enemies,teams"
     api_single_character = HTTParty.get(one_character_url)
+    name = api_single_character["response"]["results"]["name"]
+  end
+
+  def self.character_stats
+    #grab all characters that match the query
+    get_character
+
+    #grab single character
+    # one_character_url = "http://comicvine.com/api/character/4005-#{first_result_id}?api_key=#{api_key}&field_list=name,powers,id,image,team_friends,team_enemies,teams"
+    # api_single_character = HTTParty.get(one_character_url)
 
     #attributes
     name = api_single_character["response"]["results"]["name"]
@@ -22,10 +30,8 @@ module ComicVine
 
     team_enemies_hash = api_single_character["response"]["results"]["team_enemies"]["team"]
     team_enemies = team_enemies_hash.map { |enemies| enemies["name"] }
-  end
 
-  def self.character_stats
-
+    character_hash = Hash[name: name, image: image, powers: powers, friends: team_friends, enemies: team_enemies]
   end
 
   def self.api_key
