3309d32bebc3bb0ac6b64780131e76e01ceb06ff
karynntran <karynn.tran@gmail.com>
2014-12-06 18:32:31 -0500

    Add sessions and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :users, :only => [:show, :new, :create, :destroy]
+
+  delete '/sessions' => 'sessions#destroy'
+
+  get '/login' => "sessions#new"
+
+  get '/checkuser' => "sessions#check"
+
   get '/api/character' => 'api#character'
   get '/api/characters' => 'api#characters'
   get '/api/outcomes' => 'api#outcomes'
