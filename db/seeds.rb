94f89086860a88b3a6c2fd1069e3e14ec9510adc
karynntran <karynn.tran@gmail.com>
2014-12-06 13:01:37 -0500

    Add test data for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+reactions = [
+  "#{char} goes flying across the city streets!",
+  "#{char} looks extremely uncomfortable.",
+  "#{char} laughs with derision.",
+  "#{char} groans, in pain.",
+  "#{char} blocks the move!",
+  "#{char} shields just in time.",
+  "#{char} says 'Is that all you got?'",
+  "#{char} gets thrown across the street, narrowly hitting the innocent civilians!",
+  "#{char} shakes his/her fist in anger.",
+  "#{char} shakes his/her head.",
+  "#{char} feels trapped."
+]
+
+reactions.each do |reaction|
+  Reaction.create(reaction: reaction)
+end
+
+friends = [
+"#{friends} are here and they’re ready to fight.",
+"You keep calling for help, but #{friends} don’t arrive. Looks like you’re on your own for this one :( "
+]
+
+friends.each do |friend|
+  Friend.create(friend: friend)
+end
+
+outcomes = [
+  "#{char2} grimaces, retreats and says “You may have won this battle but I’ll be back!”  "
+  "This isn’t looking good! You better run…looks like #{char2} wins this round…poor civilians!"
+  "Nothing good’s gonna come out of this one. Both of you draw. Until the next battle…"
+  "#{char1} can’t go on and admits defeat. #{char2} does a victory dance and blows up more buildings."
+]
