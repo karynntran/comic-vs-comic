43e9358189547fcab02a3d4234e6be1e584406aa
karynntran <karynn.tran@gmail.com>
2014-12-06 16:22:30 -0500

    Update CV module

--- lib/modules/comic_vine.rb
+++ lib/modules/comic_vine.rb
@@ -10,48 +10,40 @@ module ComicVine
     # need to handle how characters are returned
     first_result_id = check_character_exists(api_all_characters)
 
-
     one_character_url = "http://comicvine.com/api/character/4005-#{first_result_id}?api_key=#{api_key}&field_list=name,teams,powers,id,image,team_friends,team_enemies,"
     api_single_character = HTTParty.get(one_character_url)
-  # end
 
-  # def self.character_stats
-    #get stats of character
-    # get_character(query)
-    #attributes
+  #get character stats
+
     name = api_single_character["response"]["results"]["name"]
     id = api_single_character["response"]["results"]["id"]
     image = api_single_character["response"]["results"]["image"]["medium_url"]
 
-    powers_hash = api_single_character["response"]["results"]["powers"]["power"]
+    powers_hash = api_single_character["response"]["results"]["powers"]["power"] ||= {}
     powers = powers_hash.map { |power| power["name"] }
     powers = powers.join(", ")
 
-    team_hash = api_single_character["response"]["results"]["teams"]["team"]
+    team_hash = api_single_character["response"]["results"]["teams"]["team"] ||= {}
     teams = team_hash.map { |team| team["name"] }
     teams = teams.join(", ")
 
-    team_friends_hash = api_single_character["response"]["results"]["team_friends"]["team"]
+    team_friends_hash = api_single_character["response"]["results"]["team_friends"]["team"] ||= {}
     team_friends = team_friends_hash.map { |friends| friends["name"] }
     team_friends = team_friends.join(", ")
 
-    team_enemies_hash = api_single_character["response"]["results"]["team_enemies"]["team"]
+    team_enemies_hash = api_single_character["response"]["results"]["team_enemies"]["team"] ||= {}
     team_enemies = team_enemies_hash.map { |enemies| enemies["name"] }
     team_enemies = team_enemies.join(", ")
 
     {name: name, image: image, powers: powers, friends: team_friends, enemies: team_enemies, team: teams}
   end
 
-  # def self.character_hash
-  #   {name: params[:name], image: params[:image], powers: params[:powers], friends: params[:team_friends], enemies: params[:team_enemies]}
-  # end
-
   def self.check_character_exists(api_all_chars)
     check = api_all_chars["response"]["results"]["character"][0]
       if check == nil
-        result = api_all_chars["response"]["results"]["character"][0]["id"]
+        result = api_all_chars["response"]["results"]["character"]
       else
-        result = check
+        result = api_all_chars["response"]["results"]["character"][0]
       end
       result["id"]
   end
