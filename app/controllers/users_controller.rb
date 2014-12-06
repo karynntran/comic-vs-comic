a27c2931766cd49753633b15c80d0661cf528e7a
karynntran <karynn.tran@gmail.com>
2014-12-06 18:49:32 -0500

    Add user routes and forms

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_filter :authorize, except: [:new, :create]
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = current_user
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    if params[:user]
+      @user = User.create(user_params)
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+    end
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: { status: :created, username: @user.username, id: @user.id } }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :password, :favorites)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206180813) do
+ActiveRecord::Schema.define(version: 20141206234555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,19 @@ ActiveRecord::Schema.define(version: 20141206180813) do
 
   create_table "reactions", force: true do |t|
     t.text     "reaction"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.text     "powers"
+    t.text     "friends"
+    t.boolean  "win_or_lose"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
