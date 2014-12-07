fcfb07404cad262d4a4df393e27de028dbe5672b
karynntran <karynn.tran@gmail.com>
2014-12-07 15:26:47 -0500

    Update user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+      binding.pry
       user = User.find_by({username: params[:user][:username]})
       if user && user.authenticate(params[:user][:password])
         session[:user_id] = user.id
