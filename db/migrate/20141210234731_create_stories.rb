4d225d199313af6710169b8ed2313062d8cea3e5
karynntran <karynn.tran@gmail.com>
2014-12-10 18:48:40 -0500

    Add stories table

--- db/migrate/20141210234731_create_stories.rb
+++ db/migrate/20141210234731_create_stories.rb
@@ -1,6 +1,9 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
+      t.integer :moves
+      t.boolean :completed
+      t.timestamps
     end
   end
 end
