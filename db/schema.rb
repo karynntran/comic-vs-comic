2fc984f51e61d49735200441a09041cd675ba391
karynntran <karynn.tran@gmail.com>
2014-12-11 10:59:14 -0500

    Add user_id to stories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211155350) do
+ActiveRecord::Schema.define(version: 20141211155810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20141211155350) do
     t.datetime "updated_at"
     t.string   "character_one"
     t.string   "character_two"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
