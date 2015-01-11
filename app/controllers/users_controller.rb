a2bd48f0327a4f945364f18614aa969050f038b6
karynntran <karynn.tran@gmail.com>
2015-01-11 13:14:05 -0500

    Clean up comments from Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,14 @@
 class UsersController < ApplicationController
   before_filter :authorize, except: [:show, :new, :create]
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @user = current_user
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # POST /users
-  # POST /users.json
   def create
     if params[:user]
       @user = User.create(user_params)
@@ -24,19 +19,15 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:username, :password, :image, :first_name, :last_name, :powers)
   end
